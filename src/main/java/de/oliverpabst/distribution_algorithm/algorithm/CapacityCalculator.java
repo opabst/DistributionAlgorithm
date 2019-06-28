2eeed189e75a4b586ae26db86033315514c9282c
Oliver Pabst <opabst@icloud.com>
2019-06-28 11:25:46 +0200

    - added some comments - fixed some warnings

--- src/main/java/de/oliverpabst/distribution_algorithm/algorithm/CapacityCalculator.java
+++ src/main/java/de/oliverpabst/distribution_algorithm/algorithm/CapacityCalculator.java
@@ -10,13 +10,19 @@ public class CapacityCalculator {
     private ArrayList<Integer> bucketCapacity;
 
     public CapacityCalculator(ArrayList<Integer> _weights, Integer _inputSize) {
-        bucketCapacity = new ArrayList();
+        bucketCapacity = new ArrayList<>();
+
         Integer divisor = _weights.stream().mapToInt(Integer::intValue).sum();
-        Integer remainder = _inputSize % (int)Math.floor(_inputSize / _weights.stream().mapToInt(Integer::intValue).sum());
-        for (int i = 0; i < _weights.size(); i++) {
-            bucketCapacity.add((_inputSize/divisor) * _weights.get(i));
+        int remainder = _inputSize % (int)Math.floor(_inputSize / _weights.stream().mapToInt(Integer::intValue).sum());
+
+        // initial equal distribution along the buckets considering the weights
+        for(int weight: _weights) {
+            bucketCapacity.add((_inputSize/divisor) * weight);
         }
+
         int counter = 0;
+
+        // distribute the remainder equally among the buckets
         while(remainder > 0) {
             int val = bucketCapacity.get(counter) + 1;
             bucketCapacity.set(counter, val);
